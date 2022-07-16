1e08e1e1b7badbc80ead788f847112fb967ecc1a
doinel1a <doinel1atanasiu@gmail.com>
2022-07-16 16:56:37 +0200

    Preview components in Home implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,51 @@
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
 import { NextPage } from 'next';
+import ButtonPrimary from '../components/Buttons/ButtonPrimary';
+import ButtonSecondary from '../components/Buttons/ButtonSecondary';
+import ButtonTertiary from '../components/Buttons/ButtonTertiary';
 
 import MetaHead from '../components/MetaHead';
+import Heading from '../components/Text/Heading';
+import Paragraph from '../components/Text/Paragraph';
 
 const Home: NextPage = () => {
     return (
         <>
             <MetaHead />
-            <div className='w-screen h-screen flex justify-center items-center bg-blue-300'>
-                <h1 className='text-6xl'>NEXT JS TEMPLATE</h1>
-            </div>
+            <main className='w-full h-screen flex lg:justify-center bg-blue-400 overflow-auto'>
+                <div className='container h-full flex lg:justify-center lg:items-center flex-wrap gap-x-6 lg:gap-x-20 gap-y-6 px-4 py-4'>
+                    <div className='flex flex-col gap-y-6'>
+                        <Heading type='h1' text='Heading 1' />
+                        <Heading type='h2' text='Heading 2' />
+                        <Heading type='h3' text='Heading 3' />
+                        <Heading type='h4' text='Heading 4' />
+                        <Heading type='h5' text='Heading 5' />
+                        <Heading type='h6' text='Heading 6' />
+                    </div>
+                    <div className='w-96'>
+                        <Paragraph text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
+                    </div>
+                    <div className='flex flex-col gap-y-6'>
+                        <ButtonPrimary
+                            type='button'
+                            text='Button Primary'
+                            icon={faCheckCircle}
+                            paragraphCss='!text-white'
+                            iconCss='!text-white'
+                        />
+                        <ButtonSecondary
+                            type='button'
+                            text='Button Secondary'
+                            icon={faCheckCircle}
+                        />
+                        <ButtonTertiary
+                            type='button'
+                            text='Button Terniary'
+                            icon={faCheckCircle}
+                        />
+                    </div>
+                </div>
+            </main>
         </>
     );
 };
