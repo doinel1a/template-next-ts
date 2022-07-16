2038de15e0ce007a4b435150b5d6a9492c68b328
doinel1a <doinel1atanasiu@gmail.com>
2022-07-16 14:10:27 +0200

    Heading & Paragraph components implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
     content: ['./src/**/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary-dark': '#fff',
+                'primary-light': '#333',
+            },
+        },
     },
     plugins: [],
 };
